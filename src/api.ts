6f19dd1d5e38d7c4415eb9bc8f5fb6160f1c2e7d
Percy <percy@railway.app>
2024-01-10 12:04:12 +0000

    Initial Commit

---
+++ src/api.ts
@@ -0,0 +1,45 @@
+import useSWR, { mutate } from "swr";
+import { Todo } from "./types";
+
+const fetcher = (input: RequestInfo, init?: RequestInit) =>
+  fetch(input, init).then((res) => res.json());
+
+const todoPath = "/api/todos";
+
+export const useTodos = () => useSWR<Todo[]>(todoPath, fetcher);
+
+export const createTodo = async (text: string) => {
+  mutate(
+    todoPath,
+    todos => [{ text, completed: false, id: "new-todo" }, ...todos],
+    false,
+  );
+  await fetch(todoPath, {
+    method: "POST",
+    body: JSON.stringify({ text }),
+  });
+
+  mutate(todoPath);
+};
+
+export const toggleTodo = async (todo: Todo) => {
+  mutate(
+    todoPath,
+    todos =>
+      todos.map(t =>
+        t.id === todo.id ? { ...todo, completed: !t.completed } : t,
+      ),
+    false,
+  );
+  await fetch(`${todoPath}?todoId=${todo.id}`, {
+    method: "PUT",
+    body: JSON.stringify({ completed: !todo.completed }),
+  });
+  mutate(todoPath);
+};
+
+export const deleteTodo = async (id: string) => {
+  mutate(todoPath, todos => todos.filter(t => t.id !== id), false);
+  await fetch(`${todoPath}?todoId=${id}`, { method: "DELETE" });
+  mutate(todoPath);
+};
